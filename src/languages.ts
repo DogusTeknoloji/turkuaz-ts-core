55c08b2719e66a3cc36f12e89a729779cc2ee645
Hakan Altındiş <hakan.altindis@d-teknoloji.com.tr>
2019-12-02 16:39:01 +0300

    resource adder method is generated

---
+++ src/languages.ts
@@ -0,0 +1,4 @@
+export enum Languages {
+  Turkish = 'tr',
+  English = 'en'
+}
---
+++ src/resources/index.ts
@@ -0,0 +1,11 @@
+import { Languages } from '../languages';
+import { TurkuazApplication } from '..';
+import tr from './tr';
+import en from './en';
+
+addResource(Languages.Turkish, tr);
+addResource(Languages.Turkish, en);
+
+export function addResource(lang: Languages, jsonResource: Record<string, any>) {
+  TurkuazApplication.i18n!.messages[lang] = { ...jsonResource }
+}
